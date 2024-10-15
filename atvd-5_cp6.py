a7647b6a48132116d1bac4538dbcc60da9274a76
Marsol4 <enzogmarsola@gmail.com>
2024-10-15 20:40:09 -0300

    Renomeando o arquivo

--- atvd-5_cp6.py
+++ atvd-5_cp6.py
@@ -1,3 +1,7 @@
+# Enzo Giuseppe Marsola 556310
+# Renan Dorneles Boucault 557820
+# Cauan da Cruz Ferreira 558238
+
 # Imports ----------------------------
 import os
 import oracledb
@@ -8,7 +12,7 @@ import matplotlib.pyplot as plt
 # Cria conexão com o banco de dados
 def conectar_bd() -> oracledb.Connection:
     try:
-        conn = oracledb.connect(user="SEU_USUARIO", password="SUA_SENHA", dsn="SEU_HOSTNAME:SUA_PORTA/SEU_SID")
+        conn = oracledb.connect(user="RM556310", password="130206", dsn="oracle.fiap.com.br:1521/ORCL")
         print("Conexão aberta com sucesso!")
         return conn
     except oracledb.DatabaseError as e:
